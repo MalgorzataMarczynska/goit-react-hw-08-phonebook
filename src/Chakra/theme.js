e00c6b6ed96247ba376c5183b118d9a7dc5c9a5f
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2023-03-26 16:26:48 +0200

    border bugfix

--- src/Chakra/theme.js
+++ src/Chakra/theme.js
@@ -1,14 +1,5 @@
 import { extendTheme } from '@chakra-ui/react';
-// const colors = {
-//   brand: {
-//     900: '#0000AA',
-//     800: '#808080',
-//     700: '#D3D3D3',
-//     600: '#CFEBF7',
-//     500: '#F0F8FF',
-//     400: '#FFFFFF',
-//   },
-// };
+
 const config = {
   initialColorMode: 'system',
   useSystemColorMode: true,
